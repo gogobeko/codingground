ece4e71f7ae9677c592aca873877a22940e6d1d4
omur alcin <gogobeko@gmail.com>
2015-06-11 18:11:03 +0300

    Created by Coding Ground Client

---
+++ FromTablet/AClass.java
@@ -0,0 +1,18 @@
+public class AClass{
+    private int a ;
+    private String b ;
+    
+    public String getB(){
+        return this.b; 
+    }
+    public int getA(){
+        return this.a;
+    }
+    public void setA(int a){
+        this.a = a;
+    }
+    public void setB(String b){
+        this.b = b;
+    }
+    
+}
